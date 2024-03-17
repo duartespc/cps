cae3a53e2eb79a5a7367ae862bfa21aec32f2d03
duarte-21805799 <duarte.cambra@gmail.com>
2024-03-17 14:18:21 +0000

    turn off browser_sync

--- scripts/start.js
+++ scripts/start.js
@@ -6,6 +6,7 @@ const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_module
 concurrently([
     { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
     { 
+        open: closed,
         command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
         name: 'SB_BROWSER_SYNC', 
         prefixColor: 'bgGreen.bold',
