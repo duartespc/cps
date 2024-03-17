c32bb658ee6c60aa86b85b7217166dbac3e9d8d1
duarte-21805799 <duarte.cambra@gmail.com>
2024-03-17 15:57:05 +0000

    add e-mail in start script test

--- scripts/start.js
+++ scripts/start.js
@@ -1,7 +1,26 @@
 const concurrently = require('concurrently');
+var nodemailer = require('nodemailer');
+require("dotenv").config();
+console.log(process.env) // remove this after it's working
+
 const upath = require('upath');
 
 const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.user_email,
+      pass: process.env.user_password
+    }
+  });
+  
+  var mailOptions = {
+    from: 'duarte.cambra@gmail.com',
+    to: 'duarte.cambra@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
 
 concurrently([
     { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
@@ -17,7 +36,14 @@ concurrently([
 }).then(success, failure);
 
 function success() {
-    console.log('Success');    
+    console.log('Success'); 
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });   
 }
 
 function failure() {
